98799f9684b98a5c0583a8ab60bace1d30cf7b29
張天佑 <zhangtianyou@TienYuChangMacBookAir.local>
2016-07-31 17:01:47 +0800

    adding whole project to AddWhole branch

---
+++ web design practice/index.js
@@ -0,0 +1,61 @@
+var slideIndex = 1;
+
+$(document).ready(function(){
+	showSlides();
+	// Get the modal
+	var modal = document.getElementById('myModal');
+
+	// Get the image and insert it inside the modal - use its "alt" text as a caption
+	var img = document.getElementsByClassName('myImg');
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+	
+	for(var i = 0; i < img.length; i++) {
+
+		img[i].addEventListener("click", function(){
+
+			modal.style.display = "block";
+	    	modalImg.src = this.src;
+	    	modalImg.alt = this.alt;
+	    	captionText.innerHTML = this.alt;
+		});
+	}
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() { 
+	  modal.style.display = "none";
+	}
+
+});
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+	clearTimeout(autoPlay);
+}
+
+function currentSlide(n) {
+  	showSlides(slideIndex = n);
+  	clearTimeout(autoPlay);
+}
+
+function showSlides(n) {
+	
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1} 
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+	    slides[i].style.display = "none"; 
+	}
+	for (i = 0; i < dots.length; i++) {
+	    dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex-1].style.display = "block"; 
+	dots[slideIndex-1].className += " active";
+}
+
+
